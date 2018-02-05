5cfdc9e94794b2877c49c3cadde4e81b78034933
Nick <npaul@ltu.edu>
2018-02-05 10:39:05 -0500

    use vim plug install for normal vim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.vim/swap
 mkdir ~/.vim/backup
