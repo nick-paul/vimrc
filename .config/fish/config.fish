8650d0b526e042c4ca07614bc6236e6fa785e398
nicholas.paul <nicholas.paul@soartech.com>
2019-01-24 09:44:22 -0500

    add ranger shortcut

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,27 +2,34 @@ abbr gs git status
 abbr gaa "git add --all"
 abbr fdir "find . | grep"
 abbr aya "rlwrap java -jar ~/Documents/git/aya-lang/aya.jar -i"
+abbr icat "kitty +kitten icat"
+
+abbr rr 'ranger --choosedir=$HOME/.rangerdir; set LASTDIR (cat $HOME/.rangerdir); cd "$LASTDIR"'
 
 # pyenv
-set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
-set PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+if test -d ~/.pyenv
+    set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
+    set PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+    status --is-interactive; and source (pyenv init -|psub)
+    status --is-interactive; and source (pyenv virtualenv-init -|psub)
+end
+
+# Various path variables
 
 # blender
-set BLENDER_DIR /home/npaul/blender-2.79b-linux-glibc219-x86_64
-if test -d $BLENDER_DIR
-    set PATH $JULIA_DIR $PATH
+if test -d /home/npaul/blender-2.79b-linux-glibc219-x86_64
+    set PATH /home/npaul/blender-2.79b-linux-glibc219-x86_64 $PATH
 end
 
 # julia
-set JULIA_DIR /opt/julia-1.0.3/bin
-if test -d $JULIA_DIR
-    set PATH $JULIA_DIR $PATH
+if test -d /opt/julia-1.0.3
+    set PATH /opt/julia-1.0.3/bin $PATH
 end
 
 # cling
-set CLING_DIR /opt/cling/bin
-if test -d $CLING_DIR
-    set PATH CLING_DIR $PATH
+if test -d /opt/cling
+    set PATH /opt/cling/bin $PATH
 end
+
+# kitty
+set PATH /home/npaul/.local/kitty.app/bin $PATH
