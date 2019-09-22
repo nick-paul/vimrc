c3034f1322c35c06491742a5c7059fe360b3cad8
nicholas.paul <nicholas.paul@soartech.com>
2019-09-22 12:42:34 -0400

    add coc settings

--- cp-configs-here.sh
+++ cp-configs-here.sh
@@ -1,3 +1,4 @@
 cp ~/.config/fish/config.fish   .config/fish/config.fish
 cp ~/.config/nvim/init.vim      .config/nvim/init.vim
 cp -r ~/.config/xfce4              .config/xfce4
+cp ~/.config/nvim/coc-settings.json .config/nvim/coc-settings.json
--- cp-configs-there.sh
+++ cp-configs-there.sh
@@ -1,3 +1,4 @@
 cp .config/fish/config.fish ~/.config/fish/config.fish
 cp .config/nvim/init.vim    ~/.config/nvim/init.vim
+cp .config/nvim/coc-settings.json    ~/.config/nvim/coc-settings.json
 cp .config/nvim/init.vim    ~/.vimrc
