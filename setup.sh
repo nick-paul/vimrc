67a307de1eaf7974a7dc90fa6381b9c3553a7482
nick-paul <npaul@ltu.edu>
2020-06-20 14:56:46 -0400

    Update for ubuntu 18

--- setup.sh
+++ setup.sh
@@ -7,41 +7,13 @@ if [ "$(id -u)" == "0" ] && [ -z "$SUDO_USER" ]; then
   exit -1
 fi
 
-# enable all deb-src in sources.list
-sudo sed -i '/^#\sdeb-src /s/^#//' "/etc/apt/sources.list"
-
 sudo apt update
-sudo apt upgrade -y
+sudo apt upgrade
 
-if ! command -v nvim; then
-  sudo add-apt-repository ppa:neovim-ppa/stable
-  sudo apt update
-  sudo apt install -y neovim
-fi
-
-sudo apt install -y curl
-
-if ! command -v npm; then
-  curl -sL https://deb.nodesource.com/setup_10.x | bash -
-fi
-
-if ! command -v openvpn; then
-  wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
-  echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-  sudo apt update
-  sudo apt install -y openvpn
-fi
-
-if ! command -v fish; then
-  sudo add-apt-repository ppa:fish-shell/release-2
-  sudo apt update
-  sudo apt install -y fish
-fi
-
-sudo apt install -y \
+sudo apt install \
+  curl \
   vim \
   git \
-  nodejs \
   build-essential \
   cmake \
   xfce4 \
@@ -50,55 +22,14 @@ sudo apt install -y \
   clang \
   python \
   python3 \
-  libclang-6.0-dev \
-  clang-6.0 \
-  libreadline6-dev \
-  libsqlite3-dev \
-  liblzma-dev \
-  libbz2-dev \
-  tk8.5-dev \
-  blt-dev \
-  libgdbm-dev \
-  libssl-dev \
-  libncurses5-dev \
-  pkg-config \
-  exuberant-ctags
+  neovim \
+  python-neovim \
+  python3-neovim \
+  fonts-firacode \
+  fish \
+  tldr
 
-sudo apt build-dep -y python python3
-
-sudo -u $USER bash << EOF
-
-  set -e
-
-  mkdir -p "${HOME}/.local/src/"
-  mkdir -p "${HOME}/.local/bin/"
-
-  if [ ! -e "${HOME}/.local/bin/cquery" ]; then
-
-    if [ ! -d "${HOME}/.local/src/cquery" ]; then
-      git clone --recursive https://github.com/cquery-project/cquery.git "${HOME}/.local/src/cquery"
-    fi
-
-    rm -rf "${HOME}/.local/src/cquery/build"
-    mkdir "${HOME}/.local/src/cquery/build"
-    cd "${HOME}/.local/src/cquery/build"
-
-    CC=clang-6.0
-    CXX=clang++-6.0
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DSYSTEM_CLANG=ON -DCMAKE_PREFIX_PATH='/usr/lib/llvm-6.0/'
-    cmake --build .
-    cd ~/.local/bin
-
-    ln -s ../src/cquery/build/cquery .
-  fi
-
-  if ! grep -Fq ".local/bin" "${HOME}/.bashrc"; then
-    echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> "${HOME}/.bashrc"
-  fi
-
-EOF
-
-# Install fzf
+# fzf
 if [ ! -d "${HOME}/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
@@ -107,6 +38,3 @@ fi
 #bash font/install-nerd.sh
 #bash pyenv/install.sh
 bash vim/setup.sh
-
-wget https://github.com/tonsky/FiraCode/tree/master/distr/ttf/FiraCode-Regular.ttf
-mv FiraCode-Regular.ttf ~/Desktop
--- vim/setup.sh
+++ vim/setup.sh
@@ -14,57 +14,57 @@ fi
 # Neovim
 
 if [ ! -e "${HOME}/.local/share/nvim/site/autoload/plug.vim" ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'!
     mkdir -p ~/.config/nvim
 fi
 
 
-PYENV="${HOME}/.pyenv"
 
-if [ -d "${PYENV}" ]; then
-
-    # Temp add pyenv to path
-    PATH="${HOME}/.pyenv/bin:${PATH}"
-
-    if [ ! -d "${PYENV}/versions/2.7.15" ]; then
-        pyenv install 2.7.15
-    fi
-
-
-    if [ ! -d "${PYENV}/versions/3.6.6" ]; then
-        pyenv install 3.6.6
-    fi
-
-
-    if [ ! -d "${PYENV}/versions/neovim2" ]; then
-        pyenv virtualenv 2.7.15 neovim2
-    fi
-
-
-    if [ ! -d "${PYENV}/versions/neovim3" ]; then
-        pyenv virtualenv 3.6.6 neovim3
-    fi
-
-    export PATH="/home/nick/.pyenv/bin:$PATH"
-    eval "$(pyenv init - --no-rehash)"
-    eval "$(pyenv virtualenv-init -)"
-
-    pyenv activate neovim2
-    pip install neovim
-    pyenv which python
-
-    pyenv activate neovim3
-    pip3 install neovim
-    pyenv which python
-
-else
-    echo "Please install pyenv"
-fi
-
-# install plugins
-#nvim --headless +PlugInstall +UpdateRemotePlugins +q
-
-
-
+#PYENV="${HOME}/.pyenv"
+#
+#if [ -d "${PYENV}" ]; then
+#
+#    # Temp add pyenv to path
+#    PATH="${HOME}/.pyenv/bin:${PATH}"
+#
+#    if [ ! -d "${PYENV}/versions/2.7.15" ]; then
+#        pyenv install 2.7.15
+#    fi
+#
+#
+#    if [ ! -d "${PYENV}/versions/3.6.6" ]; then
+#        pyenv install 3.6.6
+#    fi
+#
+#
+#    if [ ! -d "${PYENV}/versions/neovim2" ]; then
+#        pyenv virtualenv 2.7.15 neovim2
+#    fi
+#
+#
+#    if [ ! -d "${PYENV}/versions/neovim3" ]; then
+#        pyenv virtualenv 3.6.6 neovim3
+#    fi
+#
+#    export PATH="/home/nick/.pyenv/bin:$PATH"
+#    eval "$(pyenv init - --no-rehash)"
+#    eval "$(pyenv virtualenv-init -)"
+#
+#    pyenv activate neovim2
+#    pip install neovim
+#    pyenv which python
+#
+#    pyenv activate neovim3
+#    pip3 install neovim
+#    pyenv which python
+#
+#else
+#    echo "Please install pyenv"
+#fi
+#
+## install plugins
+##nvim --headless +PlugInstall +UpdateRemotePlugins +q
+#
+#
+#
